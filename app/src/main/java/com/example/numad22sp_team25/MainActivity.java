7f871a104dc698c04b82acad9c9cb7571464d472
Zhongshi Luo <752828402@qq.com>
2022-03-24 15:03:26 -0400

    correct the form of server key: key=serverKey on MainActivity; add a classpath of gradle on build.gradle

--- app/src/main/java/com/example/numad22sp_team25/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_team25/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String SERVER_KEY = "AAAAJrUF7t0:APA91bGQk29x23e6kZ9c-qIKkzqThfxB_Kz4jjbPvV10wE8PT6exne4fWpR5HXxl2fgR3k7s-xbjMicTL8mbBRXeN-MX5Rial6oSkJBnTijrIcByEgj8hZK3xxh1YvTuQHXeTVeTEGaM";
+    //correct the form of server key: key=serverKey
+    private static final String SERVER_KEY = "key=AAAAJrUF7t0:APA91bGQk29x23e6kZ9c-qIKkzqThfxB_Kz4jjbPvV10wE8PT6exne4fWpR5HXxl2fgR3k7s-xbjMicTL8mbBRXeN-MX5Rial6oSkJBnTijrIcByEgj8hZK3xxh1YvTuQHXeTVeTEGaM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
