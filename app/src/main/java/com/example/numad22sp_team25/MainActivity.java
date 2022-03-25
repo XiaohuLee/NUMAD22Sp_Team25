cd065cbbdf4de0de68c584eba9e2ec1e91b7f786
FengqiQiao <qiao.fe@husky.neu.edu>
2022-03-25 13:08:06 -0700

    Add/modify root/app level dependencies
    Signed-off-by: FengqiQiao <qiao.fe@husky.neu.edu>

--- app/src/main/java/com/example/numad22sp_team25/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_team25/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //correct the form of server key: key=serverKey
     private static final String SERVER_KEY = "key=AAAAJrUF7t0:APA91bGQk29x23e6kZ9c-qIKkzqThfxB_Kz4jjbPvV10wE8PT6exne4fWpR5HXxl2fgR3k7s-xbjMicTL8mbBRXeN-MX5Rial6oSkJBnTijrIcByEgj8hZK3xxh1YvTuQHXeTVeTEGaM";
+//    private static final String CLIENT_REGISTRATION_TOKEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
